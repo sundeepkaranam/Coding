a3fbce6a9375af9e5480290702d2ded69f7cc9eb
sundeep6768 <31220464+sundeep6768@users.noreply.github.com>
2019-10-29 22:44:31 +0530

    Update ReverseLinkedList.cpp

--- ReverseLinkedList.cpp
+++ ReverseLinkedList.cpp
@@ -7,21 +7,21 @@
  * };
  */
 class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
-        if(head == NULL || head->next == NULL){
-		return head;
-    }
-	ListNode* curNode = head;
-    ListNode* prevNode =NULL;
-    ListNode* nextNode = NULL;
-        
-	while(curNode != NULL){
-        nextNode = curNode->next;
-		curNode->next = prevNode;
-		prevNode = curNode;
-		curNode = nextNode;
-	}
-    return prevNode;
+  public:
+    ListNode * reverseList(ListNode * head) {
+      if (head == NULL || head - > next == NULL) {
+        return head;
+      }
+      ListNode * curNode = head;
+      ListNode * prevNode = NULL;
+      ListNode * nextNode = NULL;
+
+      while (curNode != NULL) {
+        nextNode = curNode - > next;
+        curNode - > next = prevNode;
+        prevNode = curNode;
+        curNode = nextNode;
+      }
+      return prevNode;
     }
 };
