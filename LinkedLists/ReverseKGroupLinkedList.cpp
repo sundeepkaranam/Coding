0f8ff0318e3b3501bfd59ddfdc752fcc899535ef
Sai Sundeep Karanam <sundeepch6768@gmail.com>
2019-10-30 01:05:59 +0530

    ReverseList

---
+++ LinkedLists/ReverseKGroupLinkedList.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *reverseListForGroup(ListNode *head, int k)
+    {
+        if (head == NULL || head->next == NULL || k < 1)
+        {
+            return head;
+        }
+        int traverseK;
+        ListNode *curNode = head, *newHead = NULL, *prevNode = NULL,
+                 *nextNode = NULL, *joinNode = NULL, *tailNode = NULL;
+
+        while (curNode != NULL)
+        {
+            joinNode = curNode;
+            traverseK = k;
+            while (curNode != NULL && traverseK--)
+            {
+                nextNode = curNode->next;
+                curNode->next = prevNode;
+                prevNode = curNode;
+                curNode = nextNode;
+            }
+
+            if (newHead == NULL)
+            {
+                newHead = prevNode;
+            }
+            if (tailNode)
+            {
+                tailNode->next = prevNode;
+            }
+
+            tailNode = joinNode;
+            prevNode = NULL;
+        }
+        return newHead;
+    }
+};
